f44fc12a71c74411711e7669e8dc5646e7eb8300
renat <guitarra123primera@gmail.com>
2023-07-17 14:18:02 +0200

    Added=parola utente

--- src/org/java/lessons/javapalindrome/JavaPalindrome.java
+++ src/org/java/lessons/javapalindrome/JavaPalindrome.java
@@ -1,7 +1,19 @@
 package org.java.lessons.javapalindrome;
 
+import java.util.Scanner;
+
 public class JavaPalindrome {
     public static void main(String[] args) {
+        //Inizializzamo il nostro scanner
+        Scanner scanWord = new Scanner(System.in);
+
+        //Chiediamo qui all'utente di inserire la sua parola palindroma
+        System.out.println("Inserisci la tua parola palindroma:");
+        String wordUser = scanWord.nextLine();
+
+        //Verifichiamo se la parola dell'utente è o non è palindroma
+
+
 
     }
 }
